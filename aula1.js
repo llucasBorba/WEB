1a81410e56c72adbe2c067279e2973f7bcbca804
llucasBorba <lucasborba6075@gmail.com>
2023-08-30 22:12:26 -0300

    Exercicio 2

--- aula1.js
+++ aula1.js
@@ -1,23 +1,42 @@
-const calculadora = {
- somar: function (n1, n2){
-  return n1 + n2
- },
- 
- subtrair: function (n1, n2){
-    if(n1 > n2){ 
-       return  n1 - n2
-    }
-    if(n2 > n1)
-    return n2 - n1
- }, 
+function multiplyMatrices(matrixA, matrixB) {
+   const rowsA = matrixA.length;
+   const colsA = matrixA[0].length;
+   const rowsB = matrixB.length;
+   const colsB = matrixB[0].length;
+   
+   if (colsA !== rowsB) {
+       return;
+   }
+   
+   const result = new Array(rowsA);
+   for (let i = 0; i < rowsA; i++) {
+       result[i] = new Array(colsB).fill(0);
+   }
+   
+   for (let i = 0; i < rowsA; i++) {
+       for (let j = 0; j < colsB; j++) {
+           for (let k = 0; k < colsA; k++) {
+               result[i][j] += matrixA[i][k] * matrixB[k][j];
+           }
+       }
+   }
+   
+   return result;
+}
 
- multiplicar: function (n1, n2){
-    return n1*n2
- },
+const matrixA = [
+   [2, 3],
+   [4, 5]
+];
 
- dividir: function(n1,n2){
-    return n1/n2
- }
-};
+const matrixB = [
+   [6, 7],
+   [8, 9]
+];
 
-console.log(calculadora.somar(1,8));
+const result = multiplyMatrices(matrixA, matrixB);
+if (typeof result === "string") {
+   console.log(result);
+} else {
+   console.log(result);
+}
---
+++ aula2.js
@@ -0,0 +1,42 @@
+function multiplyMatrices(matrixA, matrixB) {
+    const rowsA = matrixA.length;
+    const colsA = matrixA[0].length;
+    const rowsB = matrixB.length;
+    const colsB = matrixB[0].length;
+    
+    if (colsA !== rowsB) {
+        return;
+    }
+    
+    const result = new Array(rowsA);
+    for (let i = 0; i < rowsA; i++) {
+        result[i] = new Array(colsB).fill(0);
+    }
+    
+    for (let i = 0; i < rowsA; i++) {
+        for (let j = 0; j < colsB; j++) {
+            for (let k = 0; k < colsA; k++) {
+                result[i][j] += matrixA[i][k] * matrixB[k][j];
+            }
+        }
+    }
+    
+    return result;
+}
+
+const matrixA = [
+    [2, 3],
+    [4, 5]
+];
+
+const matrixB = [
+    [6, 7],
+    [8, 9]
+];
+
+const result = multiplyMatrices(matrixA, matrixB);
+if (typeof result === "string") {
+    console.log(result);
+} else {
+    console.log(result);
+}
